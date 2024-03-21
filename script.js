a997674190ab14a7504a6236f07940063f03b5ff
Victor Dias Noleto <lzero1541@gmail.com>
2024-03-21 15:32:53 -0300

    funcionalidade limpar cpf

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cpfs = document.querySelectorAll('.cpf li');
 function elementsInnerText([...elements]) {
   return elements.map((element) => element.innerText);
 }
+
+function limparCPF(cpf) {
+  return cpf.replace(/\D/g, '');
+}
